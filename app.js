5566d9b2a7d03cdc8188d0b085b0c9dfd9ae9e31
Ignacio Pastor <ignacio.pastor@clearpeaks.com>
2019-05-11 20:51:33 +0400

    feat: created the basic functionality to book tickets using LUIS

---
+++ app.js
@@ -0,0 +1,90 @@
+const builder = require('botbuilder');
+const restify = require('restify');
+const conf = require('./keys.js');
+
+// Setup Restify Server
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+  console.log(`${server.name} listening to ${server.url}`);
+});
+
+const connector = new builder.ChatConnector();
+server.post('/api/messages', connector.listen());
+
+const bot = new builder.UniversalBot(connector);
+
+const luisModelUrl = `https://${conf.luisAPIHostName}/luis/v2.0/apps/${conf.luisAppId}?subscription-key=${conf.luisAPIKey}`;
+
+const recognizer = new builder.LuisRecognizer(luisModelUrl);
+const intents = new builder.IntentDialog({
+  recognizers: [recognizer]
+});
+
+bot.dialog('/', intents);
+
+intents.matches('Greet', (session, args, next) => {
+  session.send(`Hello there! I'm eva, the movie ticket booking bot. How can I help you today?`);
+});
+
+const movies = ['Avengers', 'Jurassic World', 'Rampage', 'The Incredibles 2'];
+function getMoviesToDisplay() {
+  return movies.join('\n- ');
+}
+intents.matches('ShowNowPlaying', (session, args, next) => {
+  session.send(`Sure, here is the list of the movies currently playing:${getMoviesToDisplay()}`);
+});
+
+intents.matches('BookTicket', [
+  (session, args, next) => {
+    const movieEntity = args.entities.filter((e => e.type === 'Movies'));
+    const nTicketsEntity = args.entities.filter(e => e.type === 'builtin.number');
+  
+    if (movieEntity.length > 0) {
+      session.userData.movie = movieEntity[0].resolution.values[0];
+    } else {
+      delete session.userData.movie;
+    }
+    
+    if (nTicketsEntity.length > 0) {
+      session.userData.nTickets = nTicketsEntity[0].resolution.value;
+    } else {
+      delete session.userData.nTickets;
+    }
+  
+    if (!session.userData.movie) {
+      session.beginDialog('askMovie');
+    } else {
+      next();
+    }
+  },
+  (session, args, next) => {
+    if (!session.userData.nTickets) {
+      session.beginDialog('askNoOfTickets');
+    } else {
+      next();
+    }
+  },
+  (session, args, next) => {
+    session.send(`Sure, I have booked you ${session.userData.nTickets} tickets for ${session.userData.movie}. Have fun!.`);
+  }
+]);
+
+bot.dialog('askMovie', [
+  (session, args, next) => {
+    builder.Prompts.choice(session, `What movie would you like to whatch?`, movies);
+  },
+  (session, args, next) => {
+    session.userData.movie = args.response.entity;
+    session.endDialogWithResult(args);
+  }
+]);
+
+bot.dialog('askNoOfTickets', [
+  (session, args, next) => {
+    builder.Prompts.number(session, `Great! How many tickets would you like to book?`);
+  },
+  (session, args, next) => {
+    session.userData.nTickets = args.response;
+    session.endDialogWithResult(args);
+  }
+]);
